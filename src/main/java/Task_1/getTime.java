dcb0c19e173ba6d712d175d9e70fbd4fe79c8416
Yelyzaveta Buhaichuk <bug.liza@gmail.com>
2023-04-28 11:42:06 +0300

    .

---
+++ src/main/java/Task_1/getTime.java
@@ -0,0 +1,35 @@
+package Task_1;
+
+import java.time.LocalDateTime;
+
+public class getTime {
+    public static void main(String[] args) {
+        LocalDateTime startTime = LocalDateTime.now();
+        Thread timer = new Thread(() -> {
+            while (true) {
+                LocalDateTime currentTime = LocalDateTime.now();
+                long seconds = startTime.until(currentTime, java.time.temporal.ChronoUnit.SECONDS);
+                System.out.println("З запуску програми минуло " + seconds +" секунд");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread message = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(5000);
+                    System.out.println("Минуло 5 секунд");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        timer.start();
+        message.start();
+    }
+}
