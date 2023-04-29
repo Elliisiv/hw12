1d98fe0e6d86a2b4a576e4384811da1e76c75948
Yelyzaveta Buhaichuk <44315985+LisaBugaichuk@users.noreply.github.com>
2023-04-29 13:46:51 +0300

    Add files via upload

--- src/main/java/Task_2/FizzBuzz.java
+++ src/main/java/Task_2/FizzBuzz.java
@@ -2,54 +2,104 @@ package Task_2;
 import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class FizzBuzz{
-    private int n;
+public class FizzBuzz {
+    private static final AtomicInteger CURRENT_NUMBER = new AtomicInteger(1);
     private BlockingQueue<String> queue;
 
-    public FizzBuzz(int n) {
-        this.n = n;
+    public FizzBuzz() {
         this.queue = new LinkedBlockingQueue<>();
     }
 
-    public void fizz() {
-        synchronized (queue) {
-            for (int i = 1; i <= n; i++) {
-                if (i % 3 == 0 && i % 5 != 0)
-                    queue.offer("fizz");
-
-            }
-        }
-    }
-
-
-    public void buzz() {
-        synchronized (queue) {
-            for (int i = 1; i <= n; i++) {
-                if (i % 5 == 0 && i % 3 != 0)
-                    queue.offer("buzz");
-            }
-        }
-    }
-
-    public void fizzbuzz() {
-        synchronized (queue) {
-            for (int i = 1; i <= n; i++) {
-                if (i % 5 == 0 && i % 3 == 0)
-                    queue.offer("fizzbuzz");
-            }
-        }
-    }
-
-    public synchronized void number() {
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 != 0 && i % 5 != 0)
-                queue.offer(Integer.toString(i));
-        }
+    public void fizz() throws InterruptedException {
         while (true) {
-                if (!queue.isEmpty()) {
-                    System.out.println(queue.poll());
+            synchronized (queue) {
+                if (CURRENT_NUMBER.get() % 3 == 0 && CURRENT_NUMBER.get() % 5 != 0) {
+                    queue.add("fizz");
+                    CURRENT_NUMBER.incrementAndGet();
+                    queue.notifyAll();
+                } else {
+                    try {
+                        queue.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }
+            Thread.sleep(1000);
+        }
+    }
+
+    public void buzz() throws InterruptedException {
+        while (true) {
+            synchronized (queue) {
+                if (CURRENT_NUMBER.get() % 5 == 0 && CURRENT_NUMBER.get() % 3 != 0) {
+                    queue.add("buzz");
+                    CURRENT_NUMBER.incrementAndGet();
+                    queue.notifyAll();
+                } else {
+                    try {
+                        queue.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+            Thread.sleep(1000);
+        }
+    }
+
+    public void fizzbuzz() throws InterruptedException {
+        while (true) {
+            synchronized (queue) {
+                if (CURRENT_NUMBER.get() % 5 == 0 && CURRENT_NUMBER.get() % 3 == 0) {
+                    queue.add("fizzbuzz");
+                    CURRENT_NUMBER.incrementAndGet();
+                    queue.notifyAll();
+                } else {
+                    try {
+                        queue.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+            Thread.sleep(1000);
+        }
+    }
+
+    public void number() throws InterruptedException {
+        while (true) {
+            synchronized (queue) {
+                if (CURRENT_NUMBER.get() % 5 != 0 && CURRENT_NUMBER.get() % 3 != 0) {
+                    queue.add(Integer.toString(CURRENT_NUMBER.get()));
+                    CURRENT_NUMBER.incrementAndGet();
+                    queue.notifyAll();
+                } else {
+                    try {
+                        queue.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+            Thread.sleep(1000);
+        }
+    }
+
+    public void printQueue() {
+        while (true) {
+            synchronized (queue) {
+                while (!queue.isEmpty()) {
+                    System.out.print(queue.poll() + ", ");
+                }
+                try {
+                    queue.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
--- src/main/java/Task_2/FizzBuzzTest.java
+++ src/main/java/Task_2/FizzBuzzTest.java
@@ -2,17 +2,42 @@ package Task_2;
 
 public class FizzBuzzTest {
     public static void main(String[] args) {
-        FizzBuzz fizzBuzz = new FizzBuzz(15);
-        Thread thread1 = new Thread(() -> fizzBuzz.fizz());
-        Thread thread2 = new Thread(() -> fizzBuzz.buzz());
-        Thread thread3 = new Thread(() -> fizzBuzz.fizzbuzz());
-        Thread thread4 = new Thread(() -> fizzBuzz.number());
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Thread thread1 = new Thread(() -> {
+            try {
+                fizzBuzz.fizz();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            try {
+                fizzBuzz.buzz();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread thread3 = new Thread(() -> {
+            try {
+                fizzBuzz.fizzbuzz();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread thread4 = new Thread(() -> {
+            try {
+                fizzBuzz.number();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
 
         thread1.start();
         thread2.start();
         thread3.start();
         thread4.start();
-
+        fizzBuzz.printQueue();
 
         }
 }
